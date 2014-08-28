233dbe490527cb1b3b486cfacded07702d9a8ced
Robin Schneider <ypid@riseup.net>
2014-08-28 12:04:36 +0200

    Added source of source code snippet.

--- opening_hours.js
+++ opening_hours.js
@@ -4169,6 +4169,7 @@
 			d.setDate(d.getDate()+4-(d.getDay()||7));
 			return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
 		};
+		// http://stackoverflow.com/a/16591175
 		function getDateOfISOWeek(w, y) {
 			var simple = new Date(y, 0, 1 + (w - 1) * 7);
 			var dow = simple.getDay();
