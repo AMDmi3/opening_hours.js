a98ce59866175b99ff3aba7dac75c8d1ecfe66ff
Robin Schneider <ypid@riseup.net>
2015-09-06 16:32:21 +0200

    Reworked Nominatim query.

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,9 @@
-// load data in JOSM {{{
+/* Constants {{{ */
+var nominatim_api_url = 'https://nominatim.openstreetmap.org/reverse';
+// var nominatim_api_url = 'https://open.mapquestapi.com/nominatim/v1/reverse.php';
+/* }}} */
+
+// load nominatim_data in JOSM {{{
 // Using a different way to load stuff in JOSM than https://github.com/rurseekatze/OpenLinkMap/blob/master/js/small.js
 // prevent josm remote plugin of showing message
 // FIXME: Warning in console. Encoding stuff.
@@ -26,7 +31,30 @@ function dateAtWeek(date, week) {
 }
 // }}}
 
-/* Constants {{{ */
-var nominatim_api_url = 'https://nominatim.openstreetmap.org/reverse';
-// var nominatim_api_url = 'https://open.mapquestapi.com/nominatim/v1/reverse.php';
-/* }}} */
+/*
+ * The names of countries and states are localized in OSM and opening_hours.js (holidays) so we need to get the localized names from Nominatim as well.
+ */
+function reverseGeocodeLocation(lat, lon, guessed_language_for_location, callback) {
+    var nominatim_api_url_template_query = nominatim_api_url
+        + '?format=json'
+        + '&lat=' + lat + '&lon=' + lon
+        + '&zoom=5&addressdetails=1'
+        + '&email=ypid23@aol.de';
+
+    var nominatim_api_url_query = nominatim_api_url_template_query;
+    if (typeof accept_lanaguage === 'string') {
+        nominatim_api_url_query += '&accept-language=' + guessed_language_for_location;
+    }
+
+    $.getJSON(nominatim_api_url_query, function(nominatim_data) {
+        console.log(JSON.stringify(nominatim_data, null, '\t'));
+        if (nominatim_data.address.country_code === guessed_language_for_location) {
+            callback(nominatim_data);
+        } else {
+            nominatim_api_url_query += '&accept-language=' + mapCountryToLanguage(nominatim_data.address.country_code);
+            $.getJSON(nominatim_api_url_query, function(nominatim_data) {
+                callback(nominatim_data);
+            });
+        }
+    });
+}
