2dd85422ac3c4083f31e1daa628c0c4f00c58c4d
Niels Elgaard Larsen <elgaard@agol.dk>
2015-09-07 19:23:28 +0200

    added danish holidays

--- opening_hours.js
+++ opening_hours.js
@@ -1118,6 +1118,23 @@
                 // 'Silvester'               : [ 12, 31 ],
             },
         }, // }}}
+        'dk': { // {{{
+            'PH': { // https://en.wikipedia.org/wiki/Public_holidays_in_Denmark
+                'Nytårsdag'                  : [  1,  1 ],
+                'Skærtorsdag'                : [ 'easter', -3 ],
+	        'Langfredag'                 : [ 'easter', -2 ],
+                'Påskedag'                   : [ 'easter',  0 ],
+                '2. Påskedag'                : [ 'easter',  1 ],
+                'Store Bededag'              : [ 'easter', 33 ],
+                'Kristi Himmelfartsdag'      : [ 'easter', 39 ],
+                'Pinsedag'                   : [ 'easter', 49 ],
+                '2. Pinsedag'                : [ 'easter', 50 ],
+	        'Grundlovsdag'               : [  6,  5 ],  // constituion day, Not a church holiday, most treated almost but not quite as such
+                'Juleaftensdag'              : [ 12, 24 ],  // Not an official church holiday, but by law, stores are closed
+                'Juledag'                    : [ 12, 25 ],
+	        '2. Juledag'                 : [ 12, 26 ],
+            },
+        }, // }}}
         'ca': { // {{{
             'PH': { // https://en.wikipedia.org/wiki/Public_holidays_in_Canada
                 "New Year's Day" : [  1,  1 ],
