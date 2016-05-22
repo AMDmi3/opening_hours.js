740b76776c0c499ae202679087d3cb430c0c9118
Michael Knight <dex2000@users.noreply.github.com>
2016-05-22 20:34:48 +0200

    some small typos

--- locales/core.js
+++ locales/core.js
@@ -20,19 +20,19 @@
                     'nothing': 'Der Wert enthält nichts, was ausgewertet werden könnte.',
                     'nothing useful': 'Diese Regel enthält nichts nützliches. Bitte entferne diese leere Regel.',
                     'programmers joke': 'Kann es sein, dass du ein Programmierer bist und das Hinzufügen eines Semikolons nach jedem Statement ist zwanghaft ;) ?'
-                        + ' Es ist so, dass das Semikolon in der opening_hours Syntax als Trenner für Regeln definiert ist.'
+                        + ' Es ist so, dass das Semikolon in der opening_hours-Syntax als Trenner für Regeln definiert ist.'
                         + ' Bitte verzichte an dieser Stelle auf ein Semikolon.',
                     'interpreted as year': 'Die Zahl __number__ wird als Jahr interpretiert.'
                         + ' Vermutlich ist das nicht beabsichtigt. Uhrzeiten werden als "12:00" angegeben.',
                     'rule before fallback empty': 'Die Regel vor der Fallback-Regel enthält nichts nützliches',
-                    'hour min separator': 'Bitte benutze ":" als Stunden/Minuten Trenner',
+                    'hour min separator': 'Bitte benutze ":" als Stunden/Minuten-Trenner',
                     'warnings severity': 'Der Parameter optional_conf_parm["warnings_severity"] muss eine ganze Zahl zwischen (einschließlich) 0 und (einschließlich) 7 sein.'
                         + ' Gegeben: __severity__ '
                         + ', erwartet: Eine der Zahlen: __allowed__.',
                     'optional conf parm type': 'Der optional_conf_parm Parameter hat einen unbekannten Typ.'
                         + ' Gegeben: __given__',
                     'conf param tag key missing': 'Der optional_conf_parm["tag_key"] fehlt, ist aber notwendig wegen optional_conf_parm["map_value"].',
-                    'conf param mode invalid': 'Der optional_conf_parm["mode"] Parameter ist eine ungültige Zahl.'
+                    'conf param mode invalid': 'Der optional_conf_parm["mode"]-Parameter ist eine ungültige Zahl.'
                         + ' Gegeben: __given__'
                         + ', erwartet: Eine der Zahlen: __allowed__.',
                     'conf param unkown type': 'Der optional_conf_parm["__key__"] Parameter hat einen unbekannten Typ.'
@@ -52,26 +52,26 @@
                     'months': 'Monate',
                     'weekdays': 'Wochentage',
                     'ranges': 'Zeitspannen',
-                    'default state': "Diese Regel, welche den Standard Status (d.h. geschlossen) für alle folgenden Regeln ändert, ist nicht die erste Regel."
+                    'default state': "Diese Regel, welche den Standard-Status (d.h. geschlossen) für alle folgenden Regeln ändert, ist nicht die erste Regel."
                         + " Diese Regel überschreibt alle vorherigen Regeln."
-                        + " Es kann legitim sein, den Standard Status z.B. auf geöffnet festzulegen"
+                        + " Es kann legitim sein, den Standard-Status z.B. auf geöffnet festzulegen"
                         + " und dann nur die Zeiten, zu denen geschlossen ist, anzugeben.",
                     'vague': "Diese Regel ist nicht sehr aussagekräftig, da keine Zeit angegeben wurde."
                         + " Bitte füge eine Zeitangabe oder einen Kommentar hinzu, um dies zu verbessern.",
                     'empty comment': "Du hast einen leeren Kommentar verwendet."
                         + '" Bitte schreibe entweder einen Kommentar-Text oder benutze stattdessen das Schlüsselwort "unknown".',
                     'separator_for_readability': "Du hast das optionale Symbol <separator_for_readability> an der falschen Stelle benutzt."
-                        + " Bitte lies die Syntax Spezifikation um zu sehen wo es verwendet werden kann oder entferne es.",
+                        + " Bitte lies die Syntax-Spezifikation um zu sehen, wo es verwendet werden kann, oder entferne es.",
                     'strange 24/7': 'Du hast 24/7 in einer Art verwendet, welche wahrscheinlich nicht als "24 Stunden, 7 Tage die Woche" interpretiert wird.'
                         + ' Der Richtigkeit halber solltest du "open" oder "closed"'
                         + ' für diese Regel verwenden und dann die Ausnahmen angeben um das selbe Ziel zu erreichen. So ist es klarer –'
                         + ' zum Beispiel "open; Mo 12:00-14:00 off".',
-                    'public holiday': 'Es wurde keine Regel für "PH" (feiertags) angegeben. Dies ist nicht sehr Aussagekräftig.__part2__'
+                    'public holiday': 'Es wurde keine Regel für "PH" (feiertags) angegeben. Dies ist nicht sehr aussagekräftig.__part2__'
                         + ' Bitte füge die Regel "PH off" an, wenn die Einrichtung an allen Feiertagen geschlossen ist'
                         + ' oder schreibe "Sa,Su,PH 12:00-16:00" um auszudrücken, dass Samstags, Sonntags und feiertags von 12:00-16:00 geöffnet ist.'
                         + ' Falls die Einrichtung täglich und an Feiertagen geöffnet ist, kann dies explizit mittels "Mo-Su,PH" ausgedrückt werden.'
                         + ' Wenn du dir im Unklaren bist, versuche die Öffnungszeit zu klären. Falls das nicht möglich ist, lass die Angabe weg und ignoriere diese Warnung.',
-                    'public holiday part2': ' Leider ist der "tag key" (beispielsweise "opening_hours", or "lit") in opening_hours.js nicht bekannt.'
+                    'public holiday part2': ' Leider ist der "tag key" (beispielsweise "opening_hours", oder "lit") in opening_hours.js nicht bekannt.'
                         + ' Diese Warnung betrifft nur die Keys: __keys__. Falls deine Angabe nicht für einen dieser ist, ignoriere bitte folgenden Hinweis:',
                     'switched': 'Der Selektor "__first__" wurde für eine bessere Lesbarkeit und der Vollständigkeit halber mit'
                         + ' "__second__" getauscht.',
@@ -84,12 +84,12 @@
                     'period one year+': 'Bitte verwende keine __type__-Spannen mit Periode "1".'
                         + ' Wenn du ausdrücken willst, das eine Einrichtung ab einem bestimmten Jahr immer offen ist, benutze bitte "<year>+".',
                     'period one': 'Bitte verwende keine __type__-Spannen mit Periode "1".',
-                    'month 31': "Die Tagangabe für __month__ muss zwischen 1 und 31 liegen.",
+                    'month 31': "Die Tagesangabe für __month__ muss zwischen 1 und 31 liegen.",
                     'month 30': "Der Monat __month__ hat keine 31 Tage. Der letzte Tag von __month__ ist Tag 30.",
                     'month feb': '"Der Monat __month__ hat entweder 28 oder 29 Tage (Schaltjahre)."',
                     'point in time': 'Erwarte Bindestrich (-) oder offenes Ende (+) in der Zeitspanne __calc__.'
                         + ' Um mit Zeitpunkten zu arbeiten, muss der Modus für  __libraryname__ umgestellt werden.'
-                        + ' Vielleicht falscher OSM tag verwendet?',
+                        + ' Vielleicht falsches OSM-tag verwendet?',
                     'calculation': 'Berechnung',
                     'time range continue': 'Die Zeitspanne geht nicht wie erwartet weiter',
                     'period continue': 'Die Zeitspannen-Periode geht nicht wie erwartet weiter. Beispiel "/01:30".',
@@ -112,7 +112,7 @@
                     'unexpected token weekday range': 'Unerwartes Token in Tages-Spanne: __token__',
                     'max differ': 'Es sollte keinen Grund geben, mehr als __maxdiffer__ Tage von einem __name__ abzuweichen. Wenn nötig, teile uns dies bitte mit …',
                     'adding 0': 'Addition von 0 verändert das Datum nicht. Bitte weglassen.',
-                    'unexpected token holiday': 'Unerwartet Token (in Feiertags-Auswertung): __token__',
+                    'unexpected token holiday': 'Unerwarteter Token (in Feiertags-Auswertung): __token__',
                     'no holiday defintion': '__name__ ist für das Land __cc__ nicht definiert.',
                     'no holiday defintion state': '__name__ ist für das Land __cc__ und Bundesland __state__ nicht definiert.',
                     'no country code': 'Der Ländercode fehlt. Dieser wird benötigt um die korrekten Feiertage zu bestimmen (siehe in der README wie dieser anzugeben ist)',
@@ -126,17 +126,17 @@
                         + ' Bitte umdrehen.',
                     'year past': 'Das Jahr liegt in der Vergangenheit.',
                     'unexpected token year range': 'Unerwartetes Token in der Jahres-Spanne: __token__',
-                    'week range reverse': 'Du hast eine Wochen-Spanne in umgekehrter Reihenfolge oder mehrere Jahr umfassende angegeben. Dies ist aktuell nicht unterstützt.',
+                    'week range reverse': 'Du hast eine Wochen-Spanne in umgekehrter Reihenfolge oder mehrere Jahre umfassende angegeben. Dies ist aktuell nicht unterstützt.',
                     'week negative': 'Du hast eine Kalenderwoche kleiner 1 angegeben. Korrekte Angaben sind 1-53.',
                     'week exceed': 'Du hast eine Kalenderwoche größer als 53 angegeben. Korrekte Angaben sind 1-53.',
                     'week period less than 2': 'Du hast eine Wochenperiode kleiner 2 angegeben.'
-                        + ' Wenn du die gesamte Spanne von __weekfrom__ bis __weekto__ angeben will, lasse "/__period__" einfach weg.',
+                        + ' Wenn du die gesamte Spanne von __weekfrom__ bis __weekto__ angeben willst, lasse "/__period__" einfach weg.',
                     'week period greater than 26': 'Du hast eine Wochen-Periode größer als 26 angegeben.'
                         + ' 26.5 ist die Hälfte des Maximums von 53 Wochen pro Jahr. Damit würde eine Periode größer als 26 nur einmal pro Jahr auftreten.'
                     + ' Bitte gibt den Wochen-Selektor als "week __weekfrom__" an, wenn es das ist, was du ausdrücken möchtest.',
                     'unexpected token week range': 'Unerwartetes Token in Wochen-Spanne: __token__',
                     'unexpected token month range': 'Unerwartetes Token in Monats-Spanne: __token__',
-                    'day range reverse': 'Zeitspanne in falscher Reihenfolge. Begin ist größer als Ende.',
+                    'day range reverse': 'Zeitspanne in falscher Reihenfolge. Beginn ist größer als Ende.',
                     'open end': 'Angegeben als "open end". Schließzeit wurde geraten.',
                     'date parameter needed': 'Datumsparameter nötig.',
                 },
