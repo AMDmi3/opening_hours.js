b476a30c96d678ffe438107ae8a1fcace14b35d1
Paul Rysiavets <paul@afrogleap.com>
2016-11-11 11:15:36 +0100

    #171: fixed weekNumber calculator when used with Reactive Date() object

--- opening_hours.js
+++ opening_hours.js
@@ -6728,7 +6728,7 @@
         function getWeekNumber(d) {
             // Copy date so don't modify original
             d = new Date(+d);
-            d.setHours(0,0,0);
+            d.setHours(0,0,0,0);
             // Set to nearest Thursday: current date + 4 - current day number
             // Make Sunday's day number 7
             d.setDate(d.getDate() + 4 - (d.getDay()||7));
